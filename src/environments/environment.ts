cc805a4fa36a19a3a17590d9c83e5e1962136d2a
milton cañari oscco <m.developer0307@gmail.com>
2021-09-05 11:44:06 -0500

    se modifico el archivo firebase.json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   base_url:'https://appcomerces.herokuapp.com/api',
-  heroku:'https://appcomerces.herokuapp.com/api'
+  heroku:'https://appcomerces.herokuapp.com/'
 };
 
 /*
